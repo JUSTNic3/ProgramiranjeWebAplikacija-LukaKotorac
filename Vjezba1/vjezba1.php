77ead4924f6554ea9078414f8eac19ce346bd132
JUST_Nic3 <lukakotorac05@gmail.com>
2026-03-21 13:55:46 +0100

    Vjezba1

---
+++ Vjezba1/vjezba1.php
@@ -0,0 +1,10 @@
+<?php
+    $naslov = "Vježba 1 - naslov";
+    $autor = "Luka Kotorac";
+
+    echo "<h1>$naslov</h1>";
+    echo "<p>Ovu stranicu izradio je <strong>$autor.</strong></p>";
+    echo '<a href="https://animeshopbig3.hr/" target="_blank">Posjeti BIG3 anime shop.</a>';
+?>
+
+  <!-- naziv datoteke: vjezba1.php -->
