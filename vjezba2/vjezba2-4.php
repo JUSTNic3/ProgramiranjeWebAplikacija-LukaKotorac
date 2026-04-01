cefc05eea55fc3210644abfdb9df48b6b2b8aa5e
JUST_Nic3 <lukakotorac05@gmail.com>
2026-04-01 18:08:36 +0200

    vjezba2-4

---
+++ vjezba2/vjezba2-4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+  <meta charset="utf-8">
+  <title>Vježba 2-4</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+</head>
+<body>
+  <form action="izracun.php" method="post">
+    <p>
+      <label for="a">Vrijednost a:</label>
+      <input id="a" type="number" name="a">
+    </p>
+    <p>
+      <label for="b">Vrijednost b:</label>
+      <input id="b" type="number" name="b">
+    </p>
+    <input type="submit" value="Pošalji">
+  </form>
+</body>
+</html>
